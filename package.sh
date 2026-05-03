9ed0f45c56cdc78009f0304eaf04e4b67306dbdb
Maurits van der Schee <maurits@vdschee.nl>
2026-05-03 23:46:13 +0200

    Improve windows support

--- package.sh
+++ package.sh
@@ -2,10 +2,10 @@
 #
 # go install github.com/fyne-io/fyne-cross@latest
 #
-~/go/bin/fyne-cross windows -arch=amd64
-~/go/bin/fyne-cross windows -arch=arm64
-~/go/bin/fyne-cross linux   -arch=amd64
-~/go/bin/fyne-cross linux   -arch=arm64
+~/go/bin/fyne-cross windows -arch=amd64 -tags=no_animations,gles
+~/go/bin/fyne-cross windows -arch=arm64 -tags=no_animations,gles
+~/go/bin/fyne-cross linux   -arch=amd64 -tags=no_animations,gles
+~/go/bin/fyne-cross linux   -arch=arm64 -tags=no_animations,gles
 mv fyne-cross/dist/linux-arm64/tunnel-launcher.tar.xz   fyne-cross/dist/tunnel-launcher-arm64.tar.xz
 mv fyne-cross/dist/linux-amd64/tunnel-launcher.tar.xz   fyne-cross/dist/tunnel-launcher-amd64.tar.xz
 mv fyne-cross/dist/windows-arm64/tunnel-launcher.exe.zip fyne-cross/dist/tunnel-launcher-arm64.exe.zip
